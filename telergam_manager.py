56a888d92300e048eaf64052ed884b7febc99a13
AHTOOOXA <38104846+AHTOOOXA@users.noreply.github.com>
2023-10-03 13:33:02 +0500

    hyperlinks for posts

--- telergam_manager.py
+++ telergam_manager.py
@@ -15,12 +15,12 @@ class Telegram429(Exception):
 def post_to_telegram(wine) -> bool:
     url = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
     logging.info(f"Trying to post wine {wine[0]}")
-    text = f"""<b>{wine[0]}</b>\n\n{wine[1]} ₽   <s>{wine[2]} ₽</s>\nРейтинг на Vivino: {wine[7]} <i>(число оценок: {wine[8]})</i>\n\nКупить: {wine[4]}\nСтраница на Vivino: {wine[10]}\n\n{wine[3]}"""
+    text = f"""<b>{wine[0]}</b>\n\n{wine[1]} ₽   <s>{wine[2]} ₽</s>\nРейтинг на Vivino: {wine[7]} <i>(число оценок: {wine[8]})</i>\n\n<a href="{wine[4]}">Страница в магазине</a>\n<a href="{wine[10]}">Страница на Vivino</a>\n\n{wine[3]}"""
     data = {'chat_id': CHANNEL, 'photo': wine[9], 'caption': text, 'parse_mode': 'HTML'}
     while True:
         try:
             response = requests.post(url, json=data)
-            if not response.ok:
+            if response.status_code == 429:
                 raise Telegram429
         except Telegram429:
             logging.error('Telegram429 error, sleeping for 60 sec and trying again')
